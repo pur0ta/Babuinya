85b212ae447fc6e665de0d28a363a3414995c4c6
purota <purota@outlook.com>
2023-09-01 15:04:47 +0300

    add_multiply_lists

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ def sum_lists(a, b):
 		c[i] = c[i] + b[i]
 	return c
 
+def multiply_lists(a, b):
+	if len(a) > len(b):
+		c = a.copy()
+		for i in range(len(b)):
+			c[i] = c[i] * b[i]
+	else:
+		c = b.copy()
+		for i in range(len(a)):
+			c[i] = c[i] * a[i]
+	return c
+
